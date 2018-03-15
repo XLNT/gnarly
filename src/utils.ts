6530e5662c298584b0fb52bf93490f707029401c
Mykel Pereira <me@mykelpereira.com>
2018-03-15 16:51:14 -0700

    Update splitPath to account for leading empty string

--- src/utils.ts
+++ src/utils.ts
@@ -3,6 +3,9 @@ import {
 } from './Ourbit'
 
 export const splitPath = (path: string): IPathThing => {
-  const [reducerKey, domainKey, key] = path.split('/')
+  // Since the mobx patch path gets called with a leading '/',
+  // splitting the path('/') returns an array with the first element as
+  // an empty string
+  const [emptyString, reducerKey, domainKey, key] = path.split('/')
   return { reducerKey, domainKey, key }
 }
