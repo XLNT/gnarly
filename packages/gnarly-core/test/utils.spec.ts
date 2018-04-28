38f9a82c5ab8559300044c569be49d87faf093b2
Matt Condon <mattgcondon@gmail.com>
2018-04-28 16:56:40 -0700

    fix: remove more js, fix splitPath tests

--- packages/gnarly-core/test/utils.spec.ts
+++ packages/gnarly-core/test/utils.spec.ts
@@ -9,18 +9,24 @@ describe('Utils', () => {
   const domainKey = 'ownerOf'
   const key = '0x123'
   const testString = `/${reducerKey}/${domainKey}/${key}`
+  const extraTestString = `/${reducerKey}/${domainKey}/${key}/5/6`
 
   describe('- splitPath', () => {
-    it('should return an object with three keys', () => {
+    it('should return an object with four keys', () => {
       const returnSplit = splitPath(testString)
       expect(returnSplit).to.be.an('Object')
-      expect(Object.keys(returnSplit)).to.have.lengthOf(3)
+      expect(Object.keys(returnSplit)).to.have.lengthOf(4)
     })
     it('should return reducerKey, domainKey, and key with correct values', () => {
       const returnSplit = splitPath(testString)
       expect(returnSplit.reducerKey).to.equal(reducerKey)
       expect(returnSplit.domainKey).to.equal(domainKey)
       expect(returnSplit.key).to.equal(key)
+      expect(returnSplit.extra).to.deep.equal([])
+    })
+    it('should parse extra', () => {
+      const returnSplit = splitPath(extraTestString)
+      expect(returnSplit.extra).to.deep.equal(['5', '6'])
     })
   })
 })
